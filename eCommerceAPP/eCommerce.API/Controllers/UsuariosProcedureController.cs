c12aeb6ebc0358fd5c9b7dd0da0bbb0950ec1098
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-06-15 17:49:35 -0300

    finalizando curso - store procedures

---
+++ eCommerceAPP/eCommerce.API/Controllers/UsuariosProcedureController.cs
@@ -0,0 +1,74 @@
+﻿using eCommerce.API.Models;
+using eCommerce.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosProcedureController : ControllerBase
+    {
+        private IUsuarioRepository _repository;
+        public UsuariosProcedureController()
+        {
+            _repository = new UsuarioProcedureRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_repository.Get());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _repository.Get(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Insert([FromBody] Usuario usuario)
+        {
+            try
+            {
+                _repository.Insert(usuario);
+                return Ok(usuario);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Usuario usuario)
+        {
+            try
+            {
+                _repository.Update(usuario);
+                return Ok(usuario);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ eCommerceAPP/eCommerce.API/Repositories/UsuarioProcedureRepository.cs
@@ -0,0 +1,170 @@
+﻿using eCommerce.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioProcedureRepository : IUsuarioRepository
+    {
+        private IDbConnection _connection;
+        public UsuarioProcedureRepository()
+        {
+            _connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        public List<Usuario> Get()
+        {
+            List<Usuario> usuarios = new List<Usuario>();
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = (SqlConnection)_connection;
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "SelecionarUsuarios";
+
+                _connection.Open();
+
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Usuario usuario = new Usuario();
+                    usuario.Id = dataReader.GetInt32("Id");
+                    usuario.Nome = dataReader.GetString("Nome");
+                    usuario.Email = dataReader.GetString("Email");
+                    usuario.Sexo = dataReader.GetString("Sexo");
+                    usuario.Rg = dataReader.GetString("Rg");
+                    usuario.Cpf = dataReader.GetString("Cpf");
+                    usuario.NomeMae = dataReader.GetString("NomeMae");
+                    usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
+                    usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
+
+                    usuarios.Add(usuario);
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
+
+            return usuarios;
+        }
+
+        public Usuario Get(int id)
+        {
+            try
+            {
+
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "SelecionarUsuario";
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Id", id);
+                command.Connection = (SqlConnection)_connection;
+
+                _connection.Open();
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    Usuario usuario = new Usuario();
+                    usuario.Id = dataReader.GetInt32(0);
+                    usuario.Nome = dataReader.GetString("Nome");
+                    usuario.Email = dataReader.GetString("Email");
+                    usuario.Sexo = dataReader.GetString("Sexo");
+                    usuario.Rg = dataReader.GetString("RG");
+                    usuario.Cpf = dataReader.GetString("CPF");
+                    usuario.NomeMae = dataReader.GetString("NomeMae");
+                    usuario.SituacaoCadastro = dataReader.GetString("SituacaoCadastro");
+                    usuario.DataCadastro = dataReader.GetDateTimeOffset(8);
+
+                    return usuario;
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            return null;
+        }
+
+        public void Insert(Usuario usuario)
+        {
+            _connection.Open();
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = (SqlConnection)_connection;
+                command.CommandText = "CadastrarUsuario";
+                command.CommandType = CommandType.StoredProcedure;
+
+
+                command.Parameters.AddWithValue("@nome", usuario.Nome);
+                command.Parameters.AddWithValue("@email", usuario.Email);
+                command.Parameters.AddWithValue("@sexo", usuario.Sexo);
+                command.Parameters.AddWithValue("@rg", usuario.Rg);
+                command.Parameters.AddWithValue("@cpf", usuario.Cpf);
+                command.Parameters.AddWithValue("@nomeMae", usuario.NomeMae);
+                command.Parameters.AddWithValue("@situacaoCadastro", usuario.SituacaoCadastro);
+                command.Parameters.AddWithValue("@dataCadastro", usuario.DataCadastro);
+
+
+                usuario.Id = (int)command.ExecuteScalar();
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        public void Update(Usuario usuario)
+        {
+            _connection.Open();
+
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "AtualizarUsuario";
+                command.CommandType = CommandType.StoredProcedure;
+                command.Connection = (SqlConnection)_connection;
+
+                command.Parameters.AddWithValue("@nome", usuario.Nome);
+                command.Parameters.AddWithValue("@email", usuario.Email);
+                command.Parameters.AddWithValue("@sexo", usuario.Sexo);
+                command.Parameters.AddWithValue("@rg", usuario.Rg);
+                command.Parameters.AddWithValue("@cpf", usuario.Cpf);
+                command.Parameters.AddWithValue("@nomeMae", usuario.NomeMae);
+                command.Parameters.AddWithValue("@situacaoCadastro", usuario.SituacaoCadastro);
+                command.Parameters.AddWithValue("@dataCadastro", usuario.DataCadastro);
+
+                command.Parameters.AddWithValue("@id", usuario.Id);
+
+                command.ExecuteNonQuery();
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+        public void Delete(int id)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "DeletarUsuario";
+                command.CommandType = CommandType.StoredProcedure;
+                command.Connection = (SqlConnection)_connection;
+
+                command.Parameters.AddWithValue("@id", id);
+
+                _connection.Open();
+                command.ExecuteNonQuery();
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
